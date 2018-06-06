8e133319f8df760d857cc652927d3bd9f29ba03d
Paweł Żentała <pawel.zentala@ubudu.com>
2018-06-06 17:16:36 +0200

    Simple version of RWD is done

--- front/src/js/index.js
+++ front/src/js/index.js
@@ -1,5 +1,3 @@
-// const $ = require('jquery');
-
 /** Disable selecting text */
 document.onselectstart = () => { return false };
 document.onmousedown = () => { return false };
@@ -14,87 +12,32 @@ window.addEventListener('DOMContentLoaded', () => {
   // on posterImage loaded make cover transparent
   posterImage.onload = () => helloWorld.classList.add('hello-world--transparent');
 
-  // on video loaded make hide poster
-  let videoLoaded = false;
+  // video video on mobile
+  if(window.mobilecheck()) video.classList.add('is-hidden');
+
+  // when bg video loaded...
+  let videoPlayed = false;
   video.oncanplaythrough = () => {
-    setTimeout(()=>{
-      if(!videoLoaded) {
-        video.loop = true;
-        video.play();
-        videoLoaded = true;
-        poster.classList.add('video-hero__poster--hidden');
-      }
-    }, 2.5*1000)
+    // check if wasn't played before and it is not a mobile device
+    if(!videoPlayed && !window.mobilecheck()) {
 
+      // show video
+      video.classList.remove('is-hidden');
+
+      // delay rest of actions to avoid showing controls and blink
+      setTimeout(() => {
+        video.play()
+          .then(() => {
+            videoPlayed = true;
+
+            // hide poster
+            poster.classList.add('video-hero__poster--hidden');
+          })
+          .catch((err) => console.lo(err));
+      }, 2.5*1000)
+    }
   };
-
-  // lorem ipsum
-
 });
-console.log('hello from index.js')
 
-//
-// //jQuery is required to run this code
-// $( document ).ready(function() {
-//
-//   scaleVideoContainer();
-//
-//   initBannerVideoSize('.video-container .poster img');
-//   initBannerVideoSize('.video-container .filter');
-//   initBannerVideoSize('.video-container video');
-//
-//   $(window).on('resize', function() {
-//     scaleVideoContainer();
-//     scaleBannerVideoSize('.video-container .poster img');
-//     scaleBannerVideoSize('.video-container .filter');
-//     scaleBannerVideoSize('.video-container video');
-//   });
-//
-// });
-//
-// function scaleVideoContainer() {
-//
-//   var height = $(window).height() + 5;
-//   var unitHeight = parseInt(height) + 'px';
-//   $('.homepage-hero-module').css('height',unitHeight);
-//
-// }
-//
-// function initBannerVideoSize(element){
-//
-//   $(element).each(function(){
-//     $(this).data('height', $(this).height());
-//     $(this).data('width', $(this).width());
-//   });
-//
-//   scaleBannerVideoSize(element);
-//
-// }
-//
-// function scaleBannerVideoSize(element){
-//
-//   var windowWidth = $(window).width(),
-//     windowHeight = $(window).height() + 5,
-//     videoWidth,
-//     videoHeight;
-//
-//   // console.log(windowHeight);
-//
-//   $(element).each(function(){
-//     var videoAspectRatio = $(this).data('height')/$(this).data('width');
-//
-//     $(this).width(windowWidth);
-//
-//     if(windowWidth < 1000){
-//       videoHeight = windowHeight;
-//       videoWidth = videoHeight / videoAspectRatio;
-//       $(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
-//
-//       $(this).width(videoWidth).height(videoHeight);
-//     }
-//
-//     $('.homepage-hero-module .video-container video').addClass('fadeIn animated');
-//
-//   });
-// }
-//
+/** Helpers */
+window.mobilecheck = () => { var check = false; (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera); return check; };
