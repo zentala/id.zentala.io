fc067b89bd7ddccc67a3b8219e1699556638d5b9
Paweł Żentała <zentala@gmail.com>
2021-05-09 16:28:30 +0200

    Clean up, remove Node.js server, implement GitHub Pages

--- front/tests/js/index.js
+++
@@ -1,9 +0,0 @@
-import "babel-polyfill";
-
-// import "@font-awesome/css";
-// console.log('xx')
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   document.querySelector('.page-footer .date').innerHTML = new Date().getFullYear();
-// }, false);
-
