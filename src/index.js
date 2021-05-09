30e1a4472437a0580cf325a0a30546b056450cb7
Paweł Żentała <zentala@gmail.com>
2021-05-09 17:29:09 +0200

    Switch to current Node (16.x), update and cleanup dependencies

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/* Import styles - Parcel needs it here */
+import 'font-awesome/css/font-awesome.css';
+import "@fontsource/ubuntu"
+import "@fontsource/montserrat"
+import './scss/index.scss'
+
+/* Import proper page logic in JS */
+import './js/index'
